bd19ad9842f747e4183fadc21f1d2301f9ddb1ae
praveen davis <praveendavistcr@gmail.com>
2022-01-31 22:05:28 +0530

    test

---
+++ app/src/androidTest/java/com/praveen/weatherapp/ui/WeatherViewModelTest.kt
@@ -0,0 +1,14 @@
+package com.praveen.weatherapp.ui
+
+import junit.framework.TestCase
+
+class WeatherViewModelTest : TestCase() {
+
+    public override fun setUp() {
+        super.setUp()
+    }
+
+    public override fun tearDown() {}
+
+    fun testGetWeather() {}
+}
